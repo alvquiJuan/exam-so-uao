f5c0a60e0d11772e094048553900a9474200f827
Juan Manuel Álvarez Quiñonez <juan.alvarez.quinonez@correounivalle.edu.co>
2024-04-15 11:51:59 -0500

    add lolcat to  collect_evidences2.sh

--- collect_evidences2.sh
+++ collect_evidences2.sh
@@ -6,7 +6,7 @@ read codigo_estudiante
 echo "Código de estudiante: $codigo_estudiante" > evidencias.log
 
 # Verificar si existe una interfaz de red llamada "if_parcial2"
-echo "Verificando punto 1"
+echo "Verificando punto 1" | lolcat
 if ip addr show if_parcial2 &> /dev/null
 then
     echo "La interfaz de red 'if_parcial2' existe." >> evidencias.log
@@ -17,7 +17,7 @@ then
 else
     echo "La interfaz de red 'if_parcial2' no existe." >> evidencias.log
 fi
-echo "Verificando punto 2"
+echo "Verificando punto 2" | lolcat
 # Verificar si existe un usuario llamado "usr_parcial2"
 if id -u usr_parcial2 &> /dev/null
 then
@@ -49,7 +49,7 @@ then
 else
     echo "El grupo 'grp_parcial2' no existe." >> evidencias.log
 fi
-echo "Verificando punto 3"
+echo "Verificando punto 3" | lolcat
 # Determinar si existe el servicio ping_service.service y mostrar sus características
 if systemctl --all --type=service | grep -q ping_service.service
 then
@@ -62,4 +62,4 @@ else
     echo "El servicio 'ping_service.service' no existe." >> evidencias.log
 fi
 
-echo "Las validaciones se han completado y se han recopilado en el archivo 'evidencias.log'."
+echo "Las validaciones se han completado y se han recopilado en el archivo 'evidencias.log'." | lolcat
