ca0b618cf966caf1b40a12510a04972d3ee74be8
Juan Manuel Álvarez Quiñonez <juan.alvarez.quinonez@correounivalle.edu.co>
2024-09-28 18:45:05 -0500

    update collect_evidences2.sh for midetrem 2024-02

--- collect_evidences2.sh
+++ collect_evidences2.sh
@@ -6,7 +6,7 @@ read codigo_estudiante
 echo "Código de estudiante: $codigo_estudiante" > evidencias.log
 
 # Verificar si existe una interfaz de red llamada "if_parcial2"
-echo "Verificando punto 1" | lolcat
+echo "Verificando punto 1" 
 if ip addr show if_parcial2 &> /dev/null
 then
     echo "La interfaz de red 'if_parcial2' existe." >> evidencias.log
@@ -17,7 +17,7 @@ then
 else
     echo "La interfaz de red 'if_parcial2' no existe." >> evidencias.log
 fi
-echo "Verificando punto 2" | lolcat
+echo "Verificando punto 2" 
 # Verificar si existe un usuario llamado "usr_parcial2"
 if id -u usr_parcial2 &> /dev/null
 then
@@ -49,17 +49,5 @@ then
 else
     echo "El grupo 'grp_parcial2' no existe." >> evidencias.log
 fi
-echo "Verificando punto 3" | lolcat
-# Determinar si existe el servicio ping_service.service y mostrar sus características
-if systemctl --all --type=service | grep -q ping_service.service
-then
-    echo "El servicio 'ping_service.service' existe." >> evidencias.log
 
-    # Mostrar las características del servicio ping_service.service
-    echo "Características del servicio 'ping_service.service':" >> evidencias.log
-    systemctl status ping_service.service >> evidencias.log
-else
-    echo "El servicio 'ping_service.service' no existe." >> evidencias.log
-fi
-
-echo "Las validaciones se han completado y se han recopilado en el archivo 'evidencias.log'." | lolcat
+echo "Las validaciones se han completado y se han recopilado en el archivo 'evidencias.log'." 
